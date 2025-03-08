8e8d1ee05dbcb6a3ca3d0f9980adb0bf33f8f0ab
faith <faith.henu@student.moringaschool.com>
2025-03-08 10:45:36 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function wrapAdjective(adjective = "*") {
+    return function(noun) {
+        return `You are ${adjective}${noun}${adjective}!`;
+    };
+}
